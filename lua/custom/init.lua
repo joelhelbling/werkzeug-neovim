d170def16c2c29ce14590c53906691f6a4c0f895
Joel Helbling <joel@joelhelbling.com>
2023-04-08 15:04:32 -0400

    adds my fav commands for beautifying html, xml & json

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -27,4 +27,9 @@ vim.opt.smartindent = true
 vim.opt.softtabstop = indent
 vim.opt.tabstop = indent
 
+-- Beautify HTML, XML, JSON
+vim.api.nvim_create_user_command('Thtml', ':%!tidy -q -i -html', {})
+vim.api.nvim_create_user_command('Txml',  ':%!tidy -q -i -xml', {})
+vim.api.nvim_create_user_command('Tjson', ':%!jq -M --indent 2 .', {})
+
 return {}
