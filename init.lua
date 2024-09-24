0e07e22ba836a4bdcdaa30c55dff72cfa3df2068
Joel Helbling <joel@joelhelbling.com>
2024-09-24 09:40:59 -0400

    adds crystallyne for crystal lang support

--- init.lua
+++ init.lua
@@ -34,6 +34,7 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+  'elbywan/crystalline',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
